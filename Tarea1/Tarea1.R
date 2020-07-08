46542c92dbd9e9a26a7efbeac7b703495ff30013
DrakoJM <erick.ajm1993@gmail.com>
2020-07-07 23:23:39 -0500

    Primera tarea

---
+++ Tarea1/Tarea1.R
@@ -0,0 +1,24 @@
+Ejercicio1 <- function(Seg, Anio){
+  ValorEntero <- x%/%60;
+  Segundos <- x%%60;
+  Minutos <- ValorEntero%%60;
+  ValorEntero <- ValorEntero%/%60;
+  Horas <- ValorEntero%%24;
+  ValorEntero <- ValorEntero%/%24
+  Años 
+  print(Dias)
+  
+  }
+
+Ejercicio1(250000000,2018)
+
+Ejercicio2 <- function(a,b,c){
+  b=b*-1
+  (c+b)/a
+}
+Ejercicio2(2,4,0)
+Ejercicio2(5,3,0)
+Ejercicio2(7,4,18)
+
+Ejercicio3 <- 
+Ejercicio2(1,1,1)
